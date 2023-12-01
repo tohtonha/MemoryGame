b8e1eefd1ebf7cac0cf3edfc1b7f040688c0e194
kateri <brookskateri@gmail.com>
2023-12-01 13:13:35 -0500

    add html css & js

---
+++ app.js
@@ -0,0 +1,94 @@
+document.addEventListener("DOMContentLoaded", function () {
+const gameContainer = document.querySelector("#game");
+
+let noClick = false;
+let NumCardsFlipped = 0;
+let card1 = null;
+let card2 = null;
+
+const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple"
+];
+
+function shuffle(array) {
+  let counter = array.length;
+
+  while (counter > 0) {
+    let index = Math.floor(Math.random() * counter);
+    counter--;
+
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+  return array;
+}
+
+let shuffledColors = shuffle(COLORS);
+
+function createDivsForColors(colorArray) {
+  for (let color of colorArray) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add(color);
+    newDiv.addEventListener("click", handleCardClick);
+    gameContainer.append(newDiv);
+  }
+}
+
+function handleCardClick(e) {
+  if (noClick) return;
+  if (e.target.classList.contains("flipped")) return;
+
+  let currentCard = e.target;
+  currentCard.style.backgroundColor = currentCard.classList[0];
+
+  if (!card1 || !card2) {
+    currentCard.classList.add("flipped");
+    card1 = card1 || currentCard;
+    card2 = currentCard === card1 ? null : currentCard;
+  }
+
+  if (card1 && card2) {
+    noClick = true;
+    
+    let pick1 = card1.className;
+    let pick2 = card2.className;
+
+    if (pick1 === pick2) {
+      NumCardsFlipped += 2;
+      card1.removeEventListener("click", handleCardClick);
+      card2.removeEventListener("click", handleCardClick);
+      card1 = null;
+      card2 = null;
+      noClick = false;
+    } else {
+      setTimeout(function() {
+        card1.style.backgroundColor = "";
+        card2.style.backgroundColor = "";
+        card1.classList.remove("flipped");
+        card2.classList.remove("flipped");
+        card1 = null;
+        card2 = null;
+        noClick = false;
+      }, 1000);
+    }
+  }
+
+  if (NumCardsFlipped === COLORS.length) alert("game over!");
+}
+
+button.addEventListener("click", function() {
+  window.location.reload();
+})
+
+createDivsForColors(shuffledColors);
+});
